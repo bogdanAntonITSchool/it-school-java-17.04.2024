8e69453c1a1aa09f8ed40c8729b65aed55377658
Bogdan-Alexandru Anton <bogdan-alexandru.anton@1and1.ro>
2024-07-23 09:30:57 +0300

    session 24

--- test-controller/src/main/java/ro/itschool/testcontroller/controller/UserController.java
+++ test-controller/src/main/java/ro/itschool/testcontroller/controller/UserController.java
@@ -1,9 +1,13 @@
 package ro.itschool.testcontroller.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ro.itschool.testcontroller.model.User;
+import ro.itschool.testcontroller.service.UserService;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 // http://localhost:8080/api/v1/users
@@ -12,41 +16,68 @@ import java.util.Map;
 @RequestMapping("/users")
 public class UserController {
 
-    // private List<String> users = List.of("User1", "User2", "User3", "User4", "User5");
-    private static final Map<Integer, User> users = new HashMap<>();
+    private final UserService userService;
 
-    static {
-        users.put(1,
-                User.builder()
-                        .name("User1")
-                        .age(20)
-                        .email("user1@test.com")
-                        .address("Test Address")
-                        .build());
-        users.put(2, new User("User2", 21, "user2@test.com", "Test Address 2"));
-        users.put(3, new User("User3", 22, "user3@test.com", "Test Address 3"));
-        users.put(4, new User("User4", 23, "user4@test.com", "Test Address 4"));
-        users.put(5, new User("User5", 24, "user5@test.com", "Test Address 5"));
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
     public Map<Integer, User> getUsers() {
-        return users;
+        return userService.getUsers();
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable int id) {
-        return users.get(id);
+    public ResponseEntity<ResponsePayload<User>> getUserById(@PathVariable int id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponsePayload<>(user, "User found"));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ResponsePayload<>(null, "User not found"));
+        }
     }
 
+    // http://localhost:8080/api/v1/users/[orice]/[orice]
     @GetMapping("/{id}/{name}")
     public User getUserByIdAndPrintName(@PathVariable int id, @PathVariable String name) {
         System.out.println("id: " + id + ", name: " + name);
-        return users.get(id);
+        return userService.getUserById(id);
+    }
+
+    // http://localhost:8080/api/v1/users/user?name=User1&age=21&email=user1@test.com&address=Test Address
+    @GetMapping("/user")
+    public List<User> getUserByFilter(@RequestParam(required = false) String name,
+                                      @RequestParam(required = false) Integer age,
+                                      @RequestParam(required = false) String email,
+                                      @RequestParam(required = false) String address) {
+        return userService.getUserByFilters(name, age, email, address);
     }
 
     @PostMapping
-    public void addUser(@RequestBody User user) {
-        users.put(users.size() + 1, user);
+    public ResponseEntity<String> addUser(@RequestBody User user) {
+        userService.addUser(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("User added successfully");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteUser(@RequestParam int id) {
+        userService.removeUserById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateUser(@RequestParam int id, @RequestBody User user) {
+        User currentUser = userService.getUserById(id);
+        if (currentUser != null) {
+            userService.updateAndLoadUserData(currentUser, user, id);
+            return ResponseEntity.status(HttpStatus.OK).body("User updated successfully");
+        } else {
+            userService.addUser(user, id);
+            return ResponseEntity.status(HttpStatus.CREATED).body("User added successfully");
+        }
     }
 }
